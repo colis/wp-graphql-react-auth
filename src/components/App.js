9727531078f3a1277b0e699a6030e05aa3ebf6a3
Carmine Colicino <carminecolicino@gmail.com>
2019-11-25 16:39:48 +0000

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,18 @@ import '../styles/App.css';
 
 import Login from './Login';
 import UserPage from './UserPage';
+import Header from './Header';
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={UserPage} />
-    <Route exact path="/login" component={Login} />
-  </Switch>
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Switch>
+        <Route exact path="/" component={UserPage} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </div>
+  </div>
 );
 
 export default App;
