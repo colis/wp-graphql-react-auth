4e3aa51dc52e4af8fcf825997cbc89a0c694cb25
Carmine Colicino <carminecolicino@gmail.com>
2019-12-02 20:05:34 +0000

    Move Apollo Client into a separate file

--- src/index.js
+++ src/index.js
@@ -1,40 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Client } from './shared/client';
 
 import './styles/index.css';
 
 import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter } from 'react-router-dom';
-
-const httpLink = createHttpLink({
-  uri: 'https://reactlearning.wpengine.com/graphql/',
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const authToken = localStorage.getItem('auth-token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: authToken ? `Bearer ${authToken}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
   <BrowserRouter>
-    <ApolloProvider client={client}>
+    <ApolloProvider client={Client}>
       <App />
     </ApolloProvider>
   </BrowserRouter>,
