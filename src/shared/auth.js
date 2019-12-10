54fa3fd73abc31f5872c37844c64ec1bda96e02f
Carmine Colicino <carminecolicino@gmail.com>
2019-12-10 21:51:18 +0000

    Use Apollo Link functions for refreshing authToken

--- src/shared/auth.js
+++ src/shared/auth.js
@@ -1,27 +1,21 @@
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { execute, makePromise } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
 
 import { REFRESH_TOKEN_MUTATION } from './queries';
 
-const httpLink = createHttpLink({
-  uri: 'https://reactlearning.wpengine.com/graphql/',
-});
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-});
+const uri = 'https://reactlearning.wpengine.com/graphql/';
+const link = new HttpLink({ uri });
 
 export const refreshAccessToken = jwtRefreshToken => {
-  return client
-    .mutate({
-      mutation: REFRESH_TOKEN_MUTATION,
+  return makePromise(
+    execute(link, {
+      query: REFRESH_TOKEN_MUTATION,
       variables: {
         clientMutationId: 'refreshAuthToken',
         jwtRefreshToken,
       },
-    })
+    }),
+  )
     .then(({ data }) => {
       return data.refreshJwtAuthToken.authToken;
     })
